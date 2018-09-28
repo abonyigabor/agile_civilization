ed1882371a7d1c28e590edf307f07d74866ffc63
Gabor Abonyi <abonyi.gabor@ericsson.com>
2018-09-28 17:55:07 +0200

    beta 0.55

--- js/config.js
+++ js/config.js
@@ -9,11 +9,18 @@ var featureCounter = 0;
 var diff_multi = 1.0;
 var featureList = [
     [ResData.taskTypes.Feature, "Kunyhóépítés", 20, 60, 10, 1.0, {base:{r:100,t:110,d:'Építsünk házat...',s:'house'}, bonus: {r:300,t:30,d:'Ha gyorsan elkészül, többet ér!',s:'house' }}],
+    [ResData.taskTypes.Feature, "Kunyhóépítés", 20, 60, 10, 1.0, {base:{r:100,t:110,d:'Építsünk házat...',s:'house'}, bonus: {r:300,t:30,d:'Ha gyorsan elkészül, többet ér!',s:'house' }}],
+    [ResData.taskTypes.Feature, "Rádió", 50, 20, 30, 1.0, {base:{r:200,t:100,d:'Jó reggelt Vietnám!',s:'radio'}, bonus: {r:500,t:35,d:'Ha gyorsan elkészül, többet ér!',s:'radio' }}],
     [ResData.taskTypes.Feature, "Rádió", 50, 20, 30, 1.0, {base:{r:200,t:100,d:'Jó reggelt Vietnám!',s:'radio'}, bonus: {r:500,t:35,d:'Ha gyorsan elkészül, többet ér!',s:'radio' }}],
     [ResData.taskTypes.Feature, "Tárcsás telefon", 50, 10, 40, 1.0, {base:{r:250,t:100,d:'Watson jojjon ide, szuksegem van magara (Alexander Graham Bell)',s:'oldtelefon'}, bonus: {r:700,t:15,d:'Ha gyorsan elkészül, többet ér!',s:'oldtelefon' }}],
+    [ResData.taskTypes.Feature, "Tárcsás telefon", 50, 10, 40, 1.0, {base:{r:250,t:100,d:'Watson jojjon ide, szuksegem van magara (Alexander Graham Bell)',s:'oldtelefon'}, bonus: {r:700,t:15,d:'Ha gyorsan elkészül, többet ér!',s:'oldtelefon' }}],
+    [ResData.taskTypes.Feature, "Képrögzítés", 40, 20, 25, 1.0, {base:{r:300,t:110,d:'Felvegyelek?',s:'oldcamera'}, bonus: {r:900,t:20,d:'Ha gyorsan elkészül, többet ér!',s:'oldcamera' }}],
     [ResData.taskTypes.Feature, "Képrögzítés", 40, 20, 25, 1.0, {base:{r:300,t:110,d:'Felvegyelek?',s:'oldcamera'}, bonus: {r:900,t:20,d:'Ha gyorsan elkészül, többet ér!',s:'oldcamera' }}],
     [ResData.taskTypes.Feature, "Mühold", 40, 20, 25, 1.0, {base:{r:400,t:90,d:'És még mindig mozog, a Föld!',s:'satellite'}, bonus: {r:1200,t:25,d:'Ha gyorsan elkészül, többet ér!',s:'satellite' }}],
+    [ResData.taskTypes.Feature, "Mühold", 40, 20, 25, 1.0, {base:{r:400,t:90,d:'És még mindig mozog, a Föld!',s:'satellite'}, bonus: {r:1200,t:25,d:'Ha gyorsan elkészül, többet ér!',s:'satellite' }}],
     [ResData.taskTypes.Feature, "Mobiltelefon", 40, 20, 25, 1.0, {base:{r:400,t:70,d:'Honnan tudtad hogy itt vagyok?',s:'mobile'}, bonus: {r:1200,t:35,d:'Ha gyorsan elkészül, többet ér!',s:'mobile' }}],
+    [ResData.taskTypes.Feature, "Mobiltelefon", 40, 20, 25, 1.0, {base:{r:400,t:70,d:'Honnan tudtad hogy itt vagyok?',s:'mobile'}, bonus: {r:1200,t:35,d:'Ha gyorsan elkészül, többet ér!',s:'mobile' }}],
+    [ResData.taskTypes.Feature, "Robotkutya", 45, 10, 35, 1.0, {base:{r:500,t:50,d:'Intelligens, kikapcsolhato házikedvenc',s:'robot_pet'}, bonus: {r:2000,t:45,d:'Ha gyorsan elkészül, többet ér!',s:'robot_pet' }}]
     [ResData.taskTypes.Feature, "Robotkutya", 45, 10, 35, 1.0, {base:{r:500,t:50,d:'Intelligens, kikapcsolhato házikedvenc',s:'robot_pet'}, bonus: {r:2000,t:45,d:'Ha gyorsan elkészül, többet ér!',s:'robot_pet' }}]
 ]
 
